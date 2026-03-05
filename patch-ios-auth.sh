c558a1a32537a7543816bf9dada4977239d422b6
Anizio214 <anizio214@github.com>
2026-03-05 16:20:26 -0300

    fix: usar ViewController.swift para Basic Auth no Capacitor 6

--- patch-ios-auth.sh
+++ patch-ios-auth.sh
@@ -1,41 +1,26 @@
 #!/bin/bash
-# Patch AppDelegate.swift para lidar com Basic Auth no WKWebView
+# Patch ViewController.swift para Basic Auth no WKWebView (Capacitor 6)
 
-APPDELEGATE="ios/App/App/AppDelegate.swift"
+VIEWCONTROLLER="ios/App/App/ViewController.swift"
 
-cat > "$APPDELEGATE" << 'EOF'
+cat > "$VIEWCONTROLLER" << 'EOF'
 import UIKit
 import Capacitor
 import WebKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class ViewController: CAPBridgeViewController {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {}
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-    func applicationWillTerminate(_ application: UIApplication) {}
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
-
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
-    }
-}
-
-class BasicAuthViewController: CAPBridgeViewController {
     override func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodHTTPBasic {
-            let credential = URLCredential(user: "admin", password: "Ricoy123", persistence: .forSession)
+            let credential = URLCredential(
+                user: "admin",
+                password: "Ricoy123",
+                persistence: .forSession
+            )
             completionHandler(.useCredential, credential)
         } else {
             completionHandler(.performDefaultHandling, nil)
@@ -44,4 +29,4 @@ class BasicAuthViewController: CAPBridgeViewController {
 }
 EOF
 
-echo "AppDelegate.swift atualizado com Basic Auth"
+echo "ViewController.swift atualizado com Basic Auth"
